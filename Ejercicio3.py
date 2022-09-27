75c78b2b45e99556244626847f1494879977c8ce
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-09-27 19:12:36 +0200

    COMMIT 8

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,8 +1,19 @@
 ## Ejercicio 3
 '''
-Dadas dos listas, debes generar una tercera con todos los elementos que se repitan en ellas, pero no debe repetirse ningún elemento en la nueva lista:
+Dadas dos listas, debes generar una tercera con todos los elementos que se repitan
+en ellas, pero no debe repetirse ningún elemento en la nueva lista:
 '''
 
 lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
 
 lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+
+lista_3 = []
+
+for letra in lista_1:
+    if letra in lista_2:
+        if letra not in lista_3:
+            lista_3 += letra
+        else:
+            pass
+print(lista_3)

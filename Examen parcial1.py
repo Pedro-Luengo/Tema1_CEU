9f1ce95de953c4dcd0e51329bc33db213a905149
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-10 18:27:42 +0200

    COMMIT 12

--- Examen parcial1.py
+++ Examen parcial1.py
@@ -1,5 +1,10 @@
-import math
-class Pcoordenadas:
-    def _init_(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
+import sys
+"""sys.path(0,"lugar de la ruta")"""
+
+def fibonacciR(n):
+    """Calculo de fibonacci recursivo"""
+    if(n == 0 or n == 1):
+        return n
+    else:
+        return fibonacciR(n-1) + fibonacciR(n-2)
+fibonacciR(32)

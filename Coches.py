628ca2acbce406cfd8ffd895f6e1f80c7b316196
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 17:38:48 +0200

    Commit 6

--- Coches.py
+++ Coches.py
@@ -35,14 +35,14 @@ class bicicleta (vehiculo):
     def __str__(self):
         return super().__str__() + f"{self.tipo}"
 
-    class motocicleta(bicicleta):
+class motocicleta(bicicleta):
 
-        def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
-            super().__init__(color, ruedas, tipo)
-            self.velocidad = velocidad
-            self.cilindrada = cilindrada
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        super().__init__(color, ruedas, tipo)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
-        def __str__(self):
-            return super().__str__() + f"{self.velocidad} km/h, {self.cilindrada} cc"
+    def __str__(self):
+        return super().__str__() + f"{self.velocidad} km/h, {self.cilindrada} cc"
 
     

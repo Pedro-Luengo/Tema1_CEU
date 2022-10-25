0b68611278a6ec83b7a4aa9876ad9734ee498e5c
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 15:54:03 +0200

    Commit 2

--- Productos.py
+++ Productos.py
@@ -4,4 +4,8 @@ class producto:
         self.nombre = nombre
         self.precio = precio
         self.tipo = tipo
+
+    def _str_(self):
+        return "{} {} {} {}".format(self.codigo, self.nombre, self.precio, self.tipo)
+    
         

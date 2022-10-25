9264cc31de106bd4178fa345353b11f0fa72a46b
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 16:58:16 +0200

    Commit 4

--- Alumnos.py
+++ Alumnos.py
@@ -5,11 +5,18 @@ class alumno:
         self.nota = nota
         print("El alumno se ha inscrito con exito. ")
 
+    def __str__(self):
+        return f"{self.nombre} \t {self.nota}"
+
     def calificacion(self):
         if self.nota >= 5 and self.nota<= 10:
             print("El alumno ha aprobado la asignatura. ")
-        elif self.nota <=5 and self.nota >=0:
+        elif self.nota <5 and self.nota >=0:
             print("El alumno ha suspendido la asignatura. ")
         else:
             print("El alumno no ha obtenido una calificación válida.")
-        
+    
+
+Mareque = alumno("Mareque", 4)
+print(Mareque)
+alumno.calificacion(Mareque)

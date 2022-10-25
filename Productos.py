de23bb785246f043457fc7fd6854d2fa2dbd0c6f
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 15:50:29 +0200

    Commit 1

---
+++ Productos.py
@@ -0,0 +1,7 @@
+class producto:
+    def _init_(self, codigo, nombre, precio, tipo):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.tipo = tipo
+        

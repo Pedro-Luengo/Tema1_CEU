dcd0e96e2ea5cb03f44de9e83e00ec40d133a294
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 16:17:16 +0200

    Commit 6

--- Productos.py
+++ Productos.py
@@ -1,12 +1,13 @@
 class producto:
-    def _init_(self, codigo, nombre, precio, tipo):
+
+    def __init__(self, codigo, nombre, precio, tipo):
         self.codigo = codigo
         self.nombre = nombre
         self.precio = precio
         self.tipo = tipo
 
     def _str_(self):
-        return "{} {} {} {}".format(self.codigo, self.nombre, self.precio, self.tipo)
+        return "{}\t{}\t{}\t {}".format(self.codigo, self.nombre, self.precio, self.tipo)
 
     def añadir(self):
         self.añadido = True
@@ -15,7 +16,8 @@ class producto:
         else:
             print(f"El producto: {self.nombre} no se ha añadido con exito. ")    
 
-Producto_1 = producto("#994889", "Tablet", "200€", "Electronica")
+Producto_1 = producto(codigo="#994889", nombre="Tablet", precio="200€", tipo="Electronica")
+print(Producto_1)
 print(Producto_1.nombre)
 print(type(Producto_1))
 producto.añadir(Producto_1)

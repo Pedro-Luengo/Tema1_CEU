e600e046083a9456701ad0375b9435e30322b372
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 15:57:09 +0200

    Commit 3

--- Productos.py
+++ Productos.py
@@ -7,5 +7,10 @@ class producto:
 
     def _str_(self):
         return "{} {} {} {}".format(self.codigo, self.nombre, self.precio, self.tipo)
-    
-        
+
+    def añadir(self):
+        self.añadido = True
+        if self.añadido == True:
+            print(f"El producto: {self.nombre} se ha añadido con exito. ")
+        else:
+            print(f"El producto: {self.nombre} no se ha añadido con exito. ")    

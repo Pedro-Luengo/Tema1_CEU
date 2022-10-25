9faff4f17961d5a301f7038b10464433c12f0995
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-25 17:33:37 +0200

    Commit 2

---
+++ Coches.py
@@ -0,0 +1,45 @@
+class vehiculo:
+
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return f"color {self.color}, {self.ruedas} ruedas. "
+
+class coche(vehiculo):
+
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return super().__str__() + f"{self.velocidad} km/h, {self.cilindrada} cc"
+    
+    class camioneta(coche):
+
+        def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+            super().__init__(color, ruedas, velocidad, cilindrada)
+            self.carga = carga
+
+        def __str__(self):
+            return super().__str__() + f"{self.carga} kg"
+
+    class bicicleta (vehiculo):
+
+        def __init__(self, color, ruedas, tipo):
+            super().__init__(color, ruedas)
+            self.tipo = tipo
+
+        def __str__(self):
+            return super().__str__() + f"{self.tipo}"
+
+    class motocicleta(bicicleta):
+
+        def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+            super().__init__(color, ruedas, tipo)
+            self.velocidad = velocidad
+            self.cilindrada = cilindrada
+
+        

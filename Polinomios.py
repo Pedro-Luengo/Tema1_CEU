533a338d6608e6511802cfed79619ce2bf3eec6d
Pedro-Luengo <pedroluengoperez@gmail.com>
2022-10-26 18:35:55 +0200

    Commit 2

--- Polinomios.py
+++ Polinomios.py
@@ -12,31 +12,56 @@ class datoPolinomio(object):
         self.termino = termino
     
 
-class polinomio(object):
+class polinomio(datoPolinomio):
     """Clase polinomio"""
 
     def __init__(self, termino_mayor, grado):
         """Crea un polinomio del grado cero"""
         self.termino_mayor = termino_mayor
-        self.grado = grado
+        self.grado = 0
 
-def agregar_termino(polinomio, termino, valor):
-    """Agregar un termino y un valor al polinomio"""
-    aux = Nodo()
-    dato = datoPolinomio(valor, termino)
-    aux.info = dato
-    if(termino > polinomio.grado):
-        aux.sig = polinomio.termino_mayor
-        polinomio.termino_mayor = aux
-        polinomio.grado = termino
-    else:
-        actual = polinomio.termino_mayor
-        while(actual.sig is not None and termino < actual.sig.info.termino):
-            actual = actual.sig
-        aux.sig = actual.sig
-        actual.sig = aux
+    def agregar_termino(self, termino, valor):
+        """Agregar un termino y un valor al polinomio"""
+        aux = Nodo()
+        dato = datoPolinomio(valor, termino)
+        aux.info = dato
+        if(termino > polinomio.grado):
+            aux.sig = polinomio.termino_mayor
+            polinomio.termino_mayor = aux
+            polinomio.grado = termino
+        else:
+            actual = polinomio.termino_mayor
+            while(actual.sig is not None and termino < actual.sig.info.termino):
+                actual = actual.sig
+            aux.sig = actual.sig
+            actual.sig = aux
 
-def modificar_termino(polinomio, termino, valor):
-    """Modifica un termino del polinomio"""
-    aux = polinomio.termino_mayor
-    while(aux is not None )
+    def modificar_termino(self, termino, valor):
+        """Modifica un termino del polinomio"""
+        aux = polinomio.termino_mayor
+        while(aux is not None and aux.info.termino != termino):
+            aux = aux.sig
+        aux.info.valor = valor
+
+    def obtener_valor(self, termino):
+        """Devuelve el valor de un termino del polinomio"""
+        aux = polinomio.termino_mayor
+        while(aux is not None and aux.info.termino > termino):
+            aux = aux.sig
+        if(aux is not None and aux.info.termino == termino):
+            return aux.info.valor
+        else:
+            return 0
+
+    def mostrar(self):
+        """Muestra el polinomio"""
+        aux = polinomio.termino_mayor
+        pol = ""
+        if (aux is not None):
+            while(aux is not None):
+                signo = ""
+                if(aux.info.valor>=0):
+                    signo += "+"
+                pol += signo + str(aux.info.valor) + "x^" + str(aux.info.termino)
+                aux = aux.sig
+        return pol

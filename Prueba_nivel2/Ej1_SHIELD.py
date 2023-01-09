a7520e62b44754e188ec53892a1017a324d72336
Pedro-Luengo <pedroluengoperez@gmail.com>
2023-01-09 10:19:36 +0100

    commit

--- Prueba_nivel2/Ej1_SHIELD.py
+++ Prueba_nivel2/Ej1_SHIELD.py
@@ -1,94 +1,94 @@
-class SuperheroDecisionTree:
+class ArboldecisionSuperheroe:
     def __init__(self):
-        self.tree = [
+        self.arbol = [
             ["¿Es una misión intergaláctica en equipo?", "Khan", "¿Es una misión de destrucción?", "Hulk", "¿Es una misión de defensa o recuperación?", "Capitán América", "¿Es una misión de recuperación donde sea necesario no ser detectado?", "Ant-Man", "¿Es una misión donde se requiera infiltrarse con personas del lugar?", "The Winter Soldier", "¿Es una misión que requiera planificación y manejo de tecnología avanzada?", "Iron Man", "¿Es la próxima acción para tomar y moverse rápidamente de un lugar a otro?", "Nick Fury", "Thor"]
         ]
 
-    def assign_superhero(self, mission):
+    def asignar_superheroe(self, mission):
         current_node = 0
         while True:
-            node_value = self.tree[0][current_node]
-            if node_value == "Khan":
-                return node_value
-            if node_value == "Hulk":
-                return node_value
-            if node_value == "Capitán América":
-                return node_value
-            if node_value == "Ant-Man":
-                return node_value
-            if node_value == "The Winter Soldier":
-                return node_value
-            if node_value == "Iron Man":
-                return node_value
-            if node_value == "Nick Fury":
-                return node_value
-            if node_value == "Thor":
-                return node_value
-            if node_value == "¿Es una misión intergaláctica en equipo?":
-                if mission.is_intergalactic_team_mission():
+            valor_nodo = self.arbol[0][current_node]
+            if valor_nodo == "Khan":
+                return valor_nodo
+            if valor_nodo == "Hulk":
+                return valor_nodo
+            if valor_nodo == "Capitán América":
+                return valor_nodo
+            if valor_nodo == "Ant-Man":
+                return valor_nodo
+            if valor_nodo == "The Winter Soldier":
+                return valor_nodo
+            if valor_nodo == "Iron Man":
+                return valor_nodo
+            if valor_nodo == "Nick Fury":
+                return valor_nodo
+            if valor_nodo == "Thor":
+                return valor_nodo
+            if valor_nodo == "¿Es una misión intergaláctica en equipo?":
+                if mission.es_intergalactica_equipo_mision():
                     current_node = 1
                 else:
                     current_node = 2
-            elif node_value == "¿Es una misión de destrucción?":
-                if mission.is_destruction_mission():
+            elif valor_nodo == "¿Es una misión de destrucción?":
+                if mission.es_destruccion_mision():
                     current_node = 3
                 else:
                     current_node = 4
-            elif node_value == "¿Es una misión de defensa o recuperación?":
-                if mission.is_defense_or_recovery_mission():
+            elif valor_nodo == "¿Es una misión de defensa o recuperación?":
+                if mission.es_defensa_o_recuperacion_mision():
                     current_node = 5
                 else:
                     current_node = 6
-            elif node_value == "¿Es una misión de recuperación donde sea necesario no ser detectado?":
-                if mission.is_stealth_recovery_mission():
+            elif valor_nodo == "¿Es una misión de recuperación donde sea necesario no ser detectado?":
+                if mission.es_robo_recuperacion_mision():
                     current_node = 7
                 else:
                     current_node = 8
-            elif node_value == "¿Es una misión donde se requiera infiltrarse con personas del lugar?":
-                if mission.is_infiltrate_mission():
+            elif valor_nodo == "¿Es una misión donde se requiera infiltrarse con personas del lugar?":
+                if mission.es_infirtrar_mision():
                     current_node = 9
                 else:
                     current_node = 10
-            elif node_value == "¿Es una misión que requiera planificación y manejo de tecnología avanzada?":
-                if mission.is_advanced_tech_mission():
+            elif valor_nodo == "¿Es una misión que requiera planificación y manejo de tecnología avanzada?":
+                if mission.es_tecnologia_avanzada_mision():
                     current_node = 11
                 else:
                     current_node = 12
-            elif node_value == "¿Es la próxima acción para tomar y moverse rápidamente de un lugar a otro?":
-                if mission.is_next_action_move_quickly_mission():
+            elif valor_nodo == "¿Es la próxima acción para tomar y moverse rápidamente de un lugar a otro?":
+                if mission.is_proxima_accion_moverse_rapido_mision():
                     current_node = 13
                 else:
                     current_node = 14
             
 
-class Mission:
-    def __init__(self, details):
-        self.details = details
+class Mision:
+    def __init__(self, detalles):
+        self.detalles = detalles
 
-    def is_intergalactic_team_mission(self):
-        return self.details.get('type') == 'intergalactic' and self.details.get('team')
+    def es_intergalactica_equipo_mision(self):
+        return self.detalles.get('tipo') == 'intergalactica' and self.detalles.get('equipo')
 
-    def is_destruction_mission(self):
-        return self.details.get('type') == 'destruction'
+    def es_destruccion_mision(self):
+        return self.detalles.get('tipo') == 'destruccion'
 
-    def is_defense_or_recovery_mission(self):
-        return self.details.get('type') in ['defense', 'recovery']
+    def es_defensa_o_recuperacion_mision(self):
+        return self.detalles.get('tipo') in ['defensa', 'recuperacion']
 
-    def is_stealth_recovery_mission(self):
-        return self.details.get('type') == 'recovery' and self.details.get('stealth')
+    def es_robo_recuperacion_mision(self):
+        return self.detalles.get('tipo') == 'recuperacion' and self.detalles.get('robo')
 
-    def is_infiltrate_mission(self):
-        return self.details.get('type') == 'infiltrate'
+    def es_infirtrar_mision(self):
+        return self.detalles.get('tipo') == 'infiltrar'
 
-    def is_advanced_tech_mission(self):
-        return self.details.get('type') == 'advanced_tech'
+    def es_tecnologia_avanzada_mision(self):
+        return self.detalles.get('tipo') == 'tecnologia_avanzada'
 
-    def is_next_action_move_quickly_mission(self):
-        return self.details.get('type') == 'next_action_move_quickly'
+    def is_proxima_accion_moverse_rapido_mision(self):
+        return self.detalles.get('tipo') == 'proxima_accion_movimiento_rapido'
 
 if __name__ == "__main__":
     # Asignar un superhéroe a una misión
-    tree = SuperheroDecisionTree()
-    mission = Mission({'type': 'recovery', 'team': True})
-    superhero = tree.assign_superhero(mission)
-    print(f"El superhéroe asignado a esta misión es {superhero}.")
+    arbol = ArboldecisionSuperheroe()
+    mision = Mision({'tipo': 'intergalactica', 'equipo': True})
+    superheroe = arbol.asignar_superheroe(mision)
+    print(f"El superhéroe asignado a esta misión es {superheroe}.")

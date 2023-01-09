6707bf41e6c926a355c4533d0318e5412458760a
Pedro-Luengo <pedroluengoperez@gmail.com>
2023-01-09 13:19:27 +0100

    commit

--- Prueba_nivel2/Pruebitas.py
+++ Prueba_nivel2/Pruebitas.py
@@ -1,49 +1,60 @@
-from typing import List
 
-class Request:
-    def __init__(self, name: str, universe: str, description: str):
-        self.name = name
-        self.universe = universe
-        self.description = description
-        self.priority = self.set_priority()
-    
-    def set_priority(self) -> int:
-        if self.name == "Gran Conquistador" or self.universe == "616" or "El que permanece" in self.description:
-            return 3
-        elif self.name == "Khan que todo lo sabe" or "Carnicero de Dioses" in self.description or self.universe == "838":
-            return 2
-        else:
-            return 1
+class Node:
+  def __init__(self, valor):
+    self.valor = valor
+    self.siguiente = None
 
-class QueueManager:
-    def __init__(self):
-        self.queue = []
-        self.log = []
-    
-    def add_request(self, request: Request):
-        self.queue.append(request)
-    
-    def process_next_request(self):
-        request = self.queue.pop(0)
-        if request.priority == 3:
-            self.log.append(request)
-        else:
-            # process request
-            pass
+class LinkedList:
+  def __init__(self):
+    self.head = None
+  
+  def append(self, valor):
+    nuevo_nodo = Node(valor)
+    if self.head is None:
+      self.head = nuevo_nodo
+      return
+    ultimo_nodo = self.head
+    while ultimo_nodo.siguiente:
+      ultimo_nodo = ultimo_nodo.siguiente
+    ultimo_nodo.siguiente = nuevo_nodo
+  
+  def __str__(self):
+    resultado = []
+    nodo = self.head
+    while nodo:
+      resultado.append(str(nodo.valor))
+      nodo = nodo.siguiente
+    return ' '.join(resultado)
+def minCoins(cantidad):
+  monedas = LinkedList()
+  # mientras quede una cantidad por cubrir
+  while cantidad > 0:
+    # elegir la moneda o billete más grande disponible
+    for moneda in [1000, 500, 100, 50, 20, 10, 5, 2, 1]:
+      if cantidad >= moneda:
+        monedas.append(moneda)
+        cantidad -= moneda
+        break
+  # devolver la lista enlazada de monedas y billetes elegidos
+  return monedas
 
-def run_queue_manager(queue_manager: QueueManager):
-    while queue_manager.queue:
-        queue_manager.process_next_request()
+print(minCoins(15)) # debería imprimir "10 5"
+print(minCoins(27)) # debería imprimir "20 5 2"
 
-# Ejemplo de uso
-queue_manager = QueueManager()
-queue_manager.add_request(Request("Gran Conquistador", "616", "Necesito ayuda para vencer a El que permanece"))
-queue_manager.add_request(Request("Khan que todo lo sabe", "838", "Tengo un problema con el Carnicero de Dioses"))
-queue_manager.add_request(Request("Khan X", "999", "Necesito sugerencias para conquistar mi universo"))
-run_queue_manager(queue_manager)
 
-# Ver bitácora de pedidos de mayor prioridad
-for request in queue_manager.log:
-    print(f"Solicitante: {request.name}")
-    print(f"Multiverso: {request.universe}")
-    print(f"Descripción: {request.description}")
+
+def test_minCoins():
+  # Crea una lista de pruebas y sus resultados esperados
+  test_cases = [
+    (15, [10, 5]),
+    (27, [20, 5, 2]),
+    (70, [50, 20]),
+    (121, [100, 20, 1]),
+    (0, [])
+  ]
+
+  # Itera sobre la lista de pruebas
+  for test in test_cases:
+    amount, expected_output = test
+    # Verifica que la función minCoins devuelva el resultado esperado
+    assert minCoins(amount) == expected_output
